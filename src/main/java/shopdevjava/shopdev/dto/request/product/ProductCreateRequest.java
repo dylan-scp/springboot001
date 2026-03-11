baa2b5cc0d3a0a3a9af3c67713f53c2fcacb8c6d
huanhust1998 <huanhust1998@gmail.com>
2026-03-11 21:54:44 +0700

    chore: delete product

--- src/main/java/shopdevjava/shopdev/dto/request/product/ProductCreateRequest.java
+++
@@ -1,9 +0,0 @@
-package shopdevjava.shopdev.dto.request.product;
-
-import java.math.BigDecimal;
-
-public class ProductCreateRequest {
-  private String name;
-  private double price;
-  private BigDecimal discount;
-}
